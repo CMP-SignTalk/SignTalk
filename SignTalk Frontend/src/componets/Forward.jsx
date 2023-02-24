f171c54a304ad6f15dc2e4941b66f3f5bd7852d2
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2023-02-24 14:34:10 +0200

    refactor[Frontend]: Refactor the onStop Handler

--- SignTalk Frontend/src/componets/Forward.jsx
+++ SignTalk Frontend/src/componets/Forward.jsx
@@ -6,6 +6,26 @@ import axios from 'axios';
 let flag = false;
 
 const Forward = ({ recording, setRecording }) => {
+  const onStopHandler = (blobUrl, blob) => {
+    const file = new File([blob], 'audio', { type: blob.type });
+    // Send the audio file to the backend server for processing
+    const formData = new FormData();
+    formData.append('audio', file);
+    axios
+      .post('http://127.0.0.1:5000/audio', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setRecording(false);
+    flag = true;
+  };
   return (
     <div>
       <ReactMediaRecorder
@@ -13,26 +33,7 @@ const Forward = ({ recording, setRecording }) => {
         onStart={() => {
           setRecording(true);
         }}
-        onStop={(blobUrl, blob) => {
-          const file = new File([blob], 'audio', { type: blob.type });
-          // Send the audio file to the backend server for processing
-          const formData = new FormData();
-          formData.append('audio', file);
-          axios
-            .post('http://127.0.0.1:5000/audio', formData, {
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              },
-            })
-            .then((res) => {
-              console.log(res);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-          setRecording(false);
-          flag = true;
-        }}
+        onStop={onStopHandler}
         render={({ startRecording, stopRecording, mediaBlobUrl }) => (
           <div className='flex flex-col justify-center items-center'>
             <button
